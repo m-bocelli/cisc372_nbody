a65f2d985b628c1ca1d28c9a5ce4f07bccd348e9
Mikey <michael.j.bocelli@gmail.com>
2023-12-11 19:08:18 -0500

    change: removing 3rd dimension due to nan output

--- compute.cu
+++ compute.cu
@@ -8,14 +8,13 @@ __global__ void compute_accels(vector3** d_accels, vector3* d_hPos, double* d_ma
 	int i, j, k;
 	i = blockIdx.x * blockDim.x + threadIdx.x;
 	j = blockIdx.y * blockDim.y + threadIdx.y;
-	k = threadIdx.z;
 	if (i < NUMENTITIES && j < NUMENTITIES) {
 		if (i==j) {
 			FILL_VECTOR(d_accels[i][j],0,0,0);
 		}
 		else{
 			vector3 distance;
-			distance[k]=d_hPos[i][k]-d_hPos[j][k];
+			for (k=0;k<3;k++) distance[k]=d_hPos[i][k]-d_hPos[j][k];
 			double magnitude_sq=distance[0]*distance[0]+distance[1]*distance[1]+distance[2]*distance[2];
 			double magnitude=sqrt(magnitude_sq);
 			double accelmag=-1*GRAV_CONSTANT*d_mass[j]/magnitude_sq;
@@ -49,7 +48,7 @@ __global__ void sum_columns(vector3** d_accels, vector3* d_hVel, vector3* d_hPos
 //Returns: None
 //Side Effect: Modifies the hPos and hVel arrays with the new positions and accelerations after 1 INTERVAL
 void compute() {	
-	dim3 threadsPerBlock(16,16,3);
+	dim3 threadsPerBlock(16,16);
 	dim3 numBlocks(ceil((NUMENTITIES + threadsPerBlock.x-1) / threadsPerBlock.x), ceil((NUMENTITIES + threadsPerBlock.y-1) / threadsPerBlock.y));
 	
 	compute_accels<<<numBlocks,threadsPerBlock>>>(d_accels, d_hPos, d_mass);
